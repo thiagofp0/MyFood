6dcc4cecf38698e3e28a203ad955ab95b0300d3a
Thiago <thiagofp707@gmail.com>
2022-07-31 17:06:23 -0300

    First commit

---
+++ app/src/main/java/com/thiago/myfood/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.thiago.myfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TableLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val tableLayout: TableLayout = findViewById(R.id.addTab) // Pega a referência lá do xml
+    }
+}
