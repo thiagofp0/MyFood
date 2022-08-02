dc1e9249e570beb29b0ff577d9c7b7e6f70eed87
Thiago <thiagofp707@gmail.com>
2022-08-02 18:09:19 -0300

    View Binding Working

--- app/src/main/java/com/thiago/myfood/MainActivity.kt
+++ app/src/main/java/com/thiago/myfood/MainActivity.kt
@@ -3,12 +3,49 @@ package com.thiago.myfood
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TableLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.thiago.myfood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //val tableLayout: TableLayout = findViewById(R.id.addTab) // Pega a referência lá do xml
+        setupViews()
     }
+
+    private fun setupViews(){
+        val tabLayout = binding.addTab
+        val viewPager = binding.addViewPager
+        var adapter = TabViewPageAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            tab.text = getString(adapter.tabs[position])
+        }.attach()
+    }
+}
+
+class TabViewPageAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa){
+
+    val tabs = arrayOf(R.string.restaurants, R.string.marketplaces, R.string.drinks)
+    val fragments = arrayOf(MarketplaceFragment(), MarketplaceFragment(), MarketplaceFragment())
+
+    override fun getItemCount() = fragments.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+}
+
+class MarketplaceFragment : Fragment(){
+
 }
