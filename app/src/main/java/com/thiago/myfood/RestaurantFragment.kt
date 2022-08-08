d876e7c39d08cbc8fd72e223987399f8da873519
Thiago <thiagofp707@gmail.com>
2022-08-07 23:33:23 -0300

    Lista de filtros com ícones

--- app/src/main/java/com/thiago/myfood/RestaurantFragment.kt
+++ app/src/main/java/com/thiago/myfood/RestaurantFragment.kt
@@ -10,13 +10,14 @@ class RestaurantFragment : Fragment(R.layout.fragment_restaurant){
     private var binding: FragmentRestaurantBinding? = null;
 
     private var filters = arrayOf(
-        FilterItem("Ordenar"),
-        FilterItem("Para retirar"),
-        FilterItem("Entrega Grátis"),
-        FilterItem("Vale Refeição"),
-        FilterItem("Entrega Parceria"),
-        FilterItem("Super restaurante"),
-        FilterItem("Filtros")
+        FilterItem(1,"Ordenar", closeIcon = R.drawable.ic_baseline_keyboard_arrow_down_24),
+        FilterItem(2,"Para retirar", icon = R.drawable.ic_baseline_directions_walk_24),
+        FilterItem(3,"Entrega Grátis"),
+        FilterItem(4,"Vale Refeição", closeIcon = R.drawable.ic_baseline_keyboard_arrow_down_24),
+        FilterItem(5,"Distância", closeIcon = R.drawable.ic_baseline_keyboard_arrow_down_24),
+        FilterItem(6,"Entrega Parceria"),
+        FilterItem(7,"Super restaurante"),
+        FilterItem(8,"Filtros", closeIcon = R.drawable.ic_baseline_filter_list_24)
     )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
